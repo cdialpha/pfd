8815fd3d595f2fa56bf49a6ca911c442a6dc8216
Calvin <cdialpha@gmail.com>
2026-06-26 11:32:12 -0400

    Refactor cilium deploy config / values.yaml

--- deploy/scripts/up.sh
+++ deploy/scripts/up.sh
@@ -4,30 +4,42 @@ source "$(dirname "${BASH_SOURCE[0]}")/lib.sh"
 
 load_env 
 check_files 
-require kind kubectl helm
+require kind kubectl helm cilium
+# TO DO: Require specific version minimums? 
 
 # Kind Cluster
-kind create cluster --name "$CLUSTER_NAME" --config cluster-config.yaml
 
-# Wait for CTRL-P
-kubectl wait --for=condition=Ready node --all --timeout=120s
 
-# Bootstrap
+if ! kind create cluster --quiet --name "$CLUSTER_NAME" --config cluster-config.yaml; then
+    echo "✗ cluster init failed" >&2
+    exit 1
+  fi
 
-# ArgoCD Init 
+CP_IP=$(kubectl get node -l node-role.kubernetes.io/control-plane='' \
+       -o jsonpath='{.items[0].status.addresses[?(@.type=="InternalIP")].address}')
 
-# kubectl create namespace argocd || true
-# kubectl apply -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
-# kubectl apply -f bootstrap/root-app.yaml
+# TO DO: figure out how to avoid CP_IP hardcoding. 
 
+helm repo add cilium https://helm.cilium.io/
 helm repo add argo https://argoproj.github.io/argo-helm
 helm repo update
+
+helm install cilium cilium/cilium \
+      --namespace kube-system \
+      --set kubeProxyReplacement=true \
+      --set k8sServiceHost=${CP_IP}\
+      --set k8sServicePort=6443 \
+      --set l2announcements.enabled=true
+
+# kubectl wait --for=condition=Ready node --all --timeout=120s
+cilium status --wait   # or kubectl wait
+
 helm upgrade --install argocd argo/argo-cd --namespace argocd --create-namespace
 kubectl apply -f bootstrap/root-app.yaml
 
 # TO DO init Network Infra 
-    # TO DO add CNI 
-    # Add API GW 
+
+# Add API GW 
 
 
 # TO DO Helm Install Accounts Chart from 
